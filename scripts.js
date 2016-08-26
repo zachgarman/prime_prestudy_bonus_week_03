f5e910ffde0bb3327291c6f1e564eb25d3721c42
zachgarman <zachgarman@gmail.com>
2016-08-26 12:50:35 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 //Prime bonus for week 3
 
 //Function Name		--	What it Does
-//add					Adds 5 to the number
-//multiply				Multiplies the number by 2
+//add					Adds 2 to the number
+//multiply				Multiplies the number by 3
 //square				Squares the number
 
 var add = function (number) {
-  return number + 5;
+  return number + 2;
 };
 
 var multiply = function (number) {
-  return number * 2;
+  return number * 3;
 };
 
 var square = function (number) {
